edac14b395a82216b515909043257f503ed06033
Akshat Singh <132469517+Akshat041@users.noreply.github.com>
2025-12-21 01:56:23 +0530

    add routing and favorites page to the application

---
+++ frontend/src/pages/Favorites.jsx
@@ -0,0 +1,10 @@
+function Favorites() {
+  return (
+    <div className="no-favorites">
+      <h2>No Favorite Movies Yet.</h2>
+      <p>Start adding movies to your favorite and they will show up here.</p>
+    </div>
+  );
+}
+
+export default Favorites;
