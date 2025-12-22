e6effdccc47ddcd843dcde67369196af85a774e9
Akshat Singh <132469517+Akshat041@users.noreply.github.com>
2025-12-22 20:56:25 +0530

    feat: add API service for fetching popular and search movies

---
+++ frontend/src/services/api.js
@@ -0,0 +1,18 @@
+const API_KEY = " c20a4e2441cec60779f027e33097ce6f";
+const BASE_URL = `https://api.themoviedb.org/3`;
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = response.json();
+  return data.results;
+};
